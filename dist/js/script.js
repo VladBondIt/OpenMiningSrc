dcbe24a8d97f04e6b0fc94663a59c17a4a5e4d9b
Vlad Bond <foolowin@gmail.com>
2020-10-12 18:39:49 +0300

    tbc

--- dist/js/script.js
+++ dist/js/script.js
@@ -32,3 +32,53 @@ spoilers.forEach(spoiler => {
         }
     });
 });
+
+//    !!!CALCULATOR!!!
+
+// CAKC TH VALUE
+const outputSlider = document.querySelector('.calc__slider-output-th'),
+    rangeSlider = document.querySelector('.calc__slider-th');
+
+
+outputSlider.value = rangeSlider.value;
+rangeSlider.oninput = function () {
+    outputSlider.value = this.value
+};
+
+function parsValue() {
+    let x = rangeSlider.value;
+    let color = `linear-gradient(90deg, #87b645 ${x / 100}%, #fbac00 ${x / 100}%)`;
+    rangeSlider.style.background = color;
+};
+
+outputSlider.oninput = function () {
+    rangeSlider.value = this.value
+    parsValue();
+}
+
+rangeSlider.addEventListener('mousemove', parsValue);
+
+// !!! CALC DAYS VALUE
+
+const outputSliderDay = document.querySelector('.calc__slider-output-days'),
+    rangeSliderDay = document.querySelector('.calc__slider-days');
+
+outputSliderDay.value = rangeSliderDay.value
+rangeSliderDay.oninput = function () {
+    outputSliderDay.value = this.value
+}
+
+outputSliderDay.oninput = function () {
+    rangeSliderDay.value = this.value
+}
+
+function parsDayValue() {
+    let x = rangeSliderDay.value
+    let y = Math.round(Math.pow((+x / 90 * 3), 2))
+    let z = y / 6
+    console.log(y);
+    let color = `linear-gradient(90deg, #87b645 ${z * 4.7}%, #fbac00 ${z * 4.7}%)`;
+    rangeSliderDay.style.background = color;
+}
+
+rangeSliderDay.addEventListener('mousemove', parsDayValue)
